c8ea9687c4d22c21bcca33679bb41433374fbc63
amuun <amiishsaiid1@gmail.com>
2024-12-23 08:56:34 +0300

    fir commit

---
+++ ass.js
@@ -0,0 +1,23 @@
+let name=prompt("Enter your name")
+let address=prompt("Enter your address")
+
+let number=prompt("Enter your number")
+let age=prompt("Enter your birthday")
+
+let add=2024-age
+console.log(name)
+console.log(address)
+console.log(number)
+console.log(age)
+console.log(add)
+
+
+document.write(name, "<br>")
+document.write(address, "<br>")
+document.write(number, "<br>")
+document.write(age, "<br>")
+document.write(add, "<br>")
+
+
+
+
---
+++ main.js
@@ -0,0 +1,21 @@
+//console.log("hello world!")
+
+// let age= 20;
+// document.write(age)
+// console.log(age)
+
+
+// const age= 20;
+// let name= "amuun"
+
+// var phoneNumber= 3242
+// console.log(phoneNumber)
+// console.log(name)
+
+// console.log(age)
+
+let age = 20;
+let UserName = "abdi1122"
+let today = true
+var num1 = 2.6
+console.log(age, UserName, today, num1)
